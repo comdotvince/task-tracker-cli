88371c96c69a33c9d77ad1ce6cd76a8285ebc626
vincedebug <vinceferrerasermino25@gmail.com>
2024-12-01 20:56:09 +0800

    study arrow function

--- json/json.js
+++ json/json.js
@@ -1,6 +1,32 @@
-let fruits = ["apple", 'banana', 'papaya', 1];
+const fs = require('fs');
+const { json } = require('stream/consumers');
 
-const strFruits = JSON.stringify(fruits);
+const storagePath = 'storage.json';
+
+let tasks = {
+  id: 1,
+  description: "watch tv"
+};
 
 
-console.log(strFruits);
+fs.readFile(storagePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error("error", err);
+    return;
+  }
+
+  const jsonData = JSON.parse(data);
+
+  jsonData.push(tasks);
+
+  fs.writeFile(storagePath, JSON.stringify(jsonData, null, 2), 'utf-8', (err) => {
+    
+  })
+})
+
+try {
+  fs.writeFileSync('storage.json', strFruits, 'utf-8');
+  console.log("success");
+} catch(error) {
+  console.log(error);
+}
